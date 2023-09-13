3bf0afbb8191dc7b40adae0e73f0158565b524ea
Amr Tarek Elbana <142173996+amrtarekelbana@users.noreply.github.com>
2023-09-13 10:13:15 -0700

    java notes

---
+++ java.js
@@ -0,0 +1,42 @@
+
+
+// ------- String Methods ----
+// --lenght Return characters in a string as a number
+// console.log("amr".length)-----or
+// var txt="i love java script"
+// console.log(txt.length)
+
+// --charAt()  as per index value charAt ouput
+// var txt="Hello Amr";
+// console.log(txt.charAt(0))
+
+// --Trim () method removes whitespace from both sides of a string.
+// var txt="Hello Amr  ";
+// console.log(txt.trim()) 
+
+// --Slice-- method extracts a part of a string.
+// var txt="Hello Amr  ";
+// console.log(txt.slice(0,8)) 
+
+// --toUpperCase Capital charachters
+//  var txt="Hello Amr  ";
+// console.log(txt.toUpperCase())   
+
+// ---toLowercase lower charachters
+//  var txt ="Amr"
+// console.log(txt.toLowerCase())
+
+// --tostring Returns the value of a String object
+// var txt ="Amr"
+// console.log(txt.toString())
+
+// --valueOf()
+// var txt =0101
+// console.log(txt.valueOf())
+
+// --replace    
+// var txt ="hello"
+// console.log(txt.replace("l","N"))
+
+
+
